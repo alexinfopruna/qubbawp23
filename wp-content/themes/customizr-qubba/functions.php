5122e3e24940e864abc24ef8998bf4ad9feb71ae
alex <agarci9@xtec.cat>
2023-07-02 17:41:04 +0200

    estils carousel

--- wp-content/themes/customizr-qubba/functions.php
+++ wp-content/themes/customizr-qubba/functions.php
@@ -4,7 +4,7 @@
 function my_theme_enqueue_styles() {
 
  $parent_style = 'parent-style'; // Estos son los estilos del tema padre recogidos por el tema hijo.
- wp_enqueue_style ('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css');
+ //wp_enqueue_style ('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css');
  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
  wp_enqueue_style( 'child-style',
  get_stylesheet_directory_uri() . '/style.css',
@@ -22,7 +22,7 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 add_action('wp_enqueue_scripts', 'qubba_enqueue_custom_js');
 function qubba_enqueue_custom_js() {
     wp_enqueue_script('custom', get_stylesheet_directory_uri().'/qubba.js',array('jquery'), '', true);
-    wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js');
+//    wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js');
 }
 
 
